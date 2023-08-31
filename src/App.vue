3d829b5615703d03979fc0b7018f9760052d24f3
ColluraV <valerio.18.02.89@hotmail.it>
2023-08-31 12:55:28 +0200

    started

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import TheHeader from './components/TheHeader.vue'
+</script>
+
+<template>
+  
+  <TheHeader/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,28 @@
+<script>
+
+export default{
+
+    data(){
+        return{
+
+        };
+    },
+    methods:{
+
+    },
+    mounted(){
+
+    },
+
+};
+
+</script>
+
+<template>
+  
+</template>
+
+<style lang="scss" scoped>
+@use "../src/styles/partials/variables" as *;
+
+</style>
